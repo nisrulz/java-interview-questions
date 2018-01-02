25d5dd78bfae5b0deaf716aed7a845e2302448fc
Nishant Srivastava <nisrulz@gmail.com>
2018-01-02 15:27:40 +0530

    added: problem statement code comment in fibonacci_numbers

--- fibonacci_numbers/src/com/nisrulz/github/fibonacci_numbers/Main.java
+++ fibonacci_numbers/src/com/nisrulz/github/fibonacci_numbers/Main.java
@@ -2,6 +2,15 @@ package com.nisrulz.github.fibonacci_numbers;
 
 public class Main {
 
+    /*
+
+    The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... The next number is found by
+    adding up the two numbers before it.
+
+    Write a recursive method fib(n) that returns the nth Fibonacci number. n is 0 indexed, which means that in the
+    sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ..., n == 0 should return 0 and n == 3 should return 2. Assume n is
+    less than 15.
+     */
     public static void main(String[] args) {
         int n = 10;
 
