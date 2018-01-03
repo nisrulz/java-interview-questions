89fa898447547433842ef04d71d2947aa7b3b0c3
Nishant Srivastava <nisrulz@gmail.com>
2018-01-03 11:55:43 +0530

    updated: linear_search to return index of element found

--- linear_search/src/com/nisrulz/github/linear_search/Main.java
+++ linear_search/src/com/nisrulz/github/linear_search/Main.java
@@ -5,20 +5,22 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16};
 
-        if (linear_search(arr, 20)) {
-            System.out.println("Element found!");
+        int index = linear_search(arr, 9);
+
+        if (index != -1) {
+            System.out.println("Element found at index " + index);
         } else {
             System.out.println("Element not found!");
         }
 
     }
 
-    private static boolean linear_search(int[] arr, int data) {
+    private static int linear_search(int[] arr, int data) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == data) {
-                return true;
+                return i;
             }
         }
-        return false;
+        return -1;
     }
 }
