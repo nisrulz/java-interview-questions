234b4aed16ce59e40c01caf9c72983e0768ce7af
Nishant Srivastava <nisrulz@gmail.com>
2018-01-05 23:45:52 +0530

    added: currency_formatter

---
+++ currency_formatter/src/com/nisrulz/github/currency_formatter/Main.java
@@ -0,0 +1,20 @@
+package com.nisrulz.github.currency_formatter;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double payment = 12324.134;
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(new Locale("en", "in")).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
