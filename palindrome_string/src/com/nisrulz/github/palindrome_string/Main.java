97db53dc4f891f425441e58e44e07b98f42b87ef
Nishant Srivastava <nisrulz@gmail.com>
2018-01-12 01:36:30 +0530

    added: palindrome_string

---
+++ palindrome_string/src/com/nisrulz/github/palindrome_string/Main.java
@@ -0,0 +1,23 @@
+package com.nisrulz.github.palindrome_string;
+
+public class Main {
+
+    /*
+     A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward as forward,
+     such as madam or “taco cat” or racecar.
+     */
+
+    public static void main(String[] args) {
+        String[] strArray = {"abcdcba", "abcd", "This is palindrome", "cat", "taco cat", "wow", "hello", "racecar", "madam"};
+
+        for (int i = 0; i < strArray.length; i++) {
+            System.out.println(isPalindrome(strArray[i]) ? "[Palindrome] "+ strArray[i]: "[Not Palindrome] "+ strArray[i]);
+
+        }
+    }
+
+    private static boolean isPalindrome(String str) {
+        StringBuilder strBuilder = new StringBuilder(str);
+        return strBuilder.reverse().toString().equalsIgnoreCase(str);
+    }
+}
